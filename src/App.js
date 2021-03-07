e7bf18d2b62c21c973a49564f3db6bd9103553d2
Joseph M <jmedina1985@hotmail.com>
2021-03-07 12:55:59 -0800

    Getting set up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <main>
       <section className="container">
         <h3>0 birthdays today</h3>
+        <List />
+        <button onClick={() => console.log('You clicked me')}>clear all</button>
       </section>
     </main>
   );
