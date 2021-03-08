2504118dcf074d50c303fb4bf1490b0a89d51289
Joseph M <jmedina1985@hotmail.com>
2021-03-07 22:40:17 -0800

    getting list component working

--- src/List.js
+++ src/List.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+  return <>{people.map((person) => {})}</>;
 };
 
 export default List;
