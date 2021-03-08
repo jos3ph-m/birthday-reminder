7d298dd6205cb93f73f0fe9fc079346ecdbceaf4
Joseph M <jmedina1985@hotmail.com>
2021-03-07 22:41:49 -0800

    getting list component working

--- src/List.js
+++ src/List.js
@@ -8,6 +8,10 @@ const List = ({ people }) => {
         return (
           <article key={id} className="person">
             <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
           </article>
         );
       })}
