33c84b25506e0994724ceda43e01c08f880f8fd6
Joseph M <jmedina1985@hotmail.com>
2021-03-07 22:41:22 -0800

    getting list component working

--- src/List.js
+++ src/List.js
@@ -5,7 +5,11 @@ const List = ({ people }) => {
     <>
       {people.map((person) => {
         const { id, name, age, image } = person;
-        return <article key={id}></article>;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+          </article>
+        );
       })}
     </>
   );
